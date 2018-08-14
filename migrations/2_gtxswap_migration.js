5add84a2b6296ebcc59651867b091a01b4859afb
ToniyaSundaram <toniya.sundaram@gmail.com>
2018-08-14 16:15:10 +0800

    converting constructor parameters to functions

--- migrations/2_gtxswap_migration.js
+++
@@ -1,8 +0,0 @@
-var GTXSwap = artifacts.require("./GTXSwap.sol");
-var SafeMath = artifacts.require("./SafeMath.sol");
-
-module.exports = function (deployer) {
-    deployer.deploy(SafeMath);
-    deployer.link(SafeMath,GTXSwap);
-    deployer.deploy(GTXSwap,1);
-};
--- migrations/3_finmigrate_migration.js
+++
@@ -1,8 +0,0 @@
-var FINMigrate = artifacts.require("./FINMigrate.sol");
-var SafeMath = artifacts.require("./SafeMath.sol");
-
-module.exports = function (deployer) {
-    deployer.deploy(SafeMath);
-    deployer.link(SafeMath,FINMigrate);
-    deployer.deploy(FINMigrate,100);
-};
